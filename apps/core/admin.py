047348ee0ff36f636e5cc40085b73c7198b04d39
Gustavo Carvalho <gt.salles@gmail.com>
2013-09-25 21:37:37 -0300

    Add sample model e a few admin features

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,1 +1,15 @@
-__author__ = 'gustavo'
+from django.contrib import admin
+from .models import CommonUser
+
+
+class CommomUserAdmin(admin.ModelAdmin):
+    """
+    ModelAdmin for Class CommomUser
+    """
+
+    list_display = ('user', 'name', 'email', 'phone', 'active')
+    list_display_links = ('user', 'name')
+    list_editable = ('active',)
+
+
+admin.site.register(CommonUser, CommomUserAdmin)
--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class CommonUser(models.Model):
+    """
+    A Commom User
+    """
+
+    user = models.ForeignKey(User)
+    name = models.CharField(_(u'Name'), max_length=30)
+    email = models.EmailField()
+    phone = models.CharField(_(u'Telephone'), max_length=11, blank=True)
+    address = models.CharField(_(u'Address'), max_length=75, blank=True)
+    active = models.BooleanField(_(u'Active'), default=True)
+    date_joined = models.DateTimeField(_(u'Member Since'), auto_now_add=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _(u'Commom User')
